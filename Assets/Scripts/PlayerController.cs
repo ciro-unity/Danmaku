ffc305c4a78167bcd37afaa7568762dcaf40391d
Ciro Continisio <ciro@unity3d.com>
2018-03-11 19:30:53 +0000

    Ship texture, Ship animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,15 @@ public class PlayerController : MonoBehaviour
 	public float speed = 5f, maxSpeed = 100f;
 	
 	private new Rigidbody2D rigidbody2D;
+	private Animator animator;
 	private Vector2 input, inputRaw;
 	private Vector2 speedOperations;
+	private bool isShooting = false;
 
 	void Start ()
 	{
 		rigidbody2D = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
 	
 	void Update ()
@@ -21,6 +24,11 @@ public class PlayerController : MonoBehaviour
 		input.y = Input.GetAxis("Vertical");
 		inputRaw.x = Input.GetAxisRaw("Horizontal");
 		inputRaw.y = Input.GetAxisRaw("Vertical");
+		isShooting = Input.GetButton("Fire1");
+
+		animator.SetFloat("HorizontalMovement", input.x);
+		animator.SetFloat("VerticalMovement", input.y);
+		animator.SetBool("IsShooting", isShooting);
 	}
 
 	private void FixedUpdate()
